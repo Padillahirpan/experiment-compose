4b471622da38249f2ea7e4a64db0e21b758d6405
Irpan Padillah <irpan@argapro.id>
2023-10-23 23:51:19 +0700

    add library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,11 @@ dependencies {
     implementation("androidx.work:work-runtime-ktx:2.8.1")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
 }
